aebe85a0379a5aebecbedd247f1a4001f9b03ff7
Mark <manho445@gmail.com>
2022-01-15 17:36:49 +0800

    firebase set Up(100%)

---
+++ FypTest_APP/viewController/SearchMapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchMapViewController.swift
+//  FypTest_APP
+//
+//  Created by kin ming ching on 15/1/2022.
+//
+
+import UIKit
+
+class SearchMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
