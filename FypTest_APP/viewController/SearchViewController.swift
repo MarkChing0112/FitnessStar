33afe548bc3c5efef97ffa3a7a81bcaf45a5ed05
isaaclee0928 <90861812+isaaclee0928@users.noreply.github.com>
2022-01-15 23:35:03 +0800

    add search

--- FypTest_APP/viewController/SearchViewController.swift
+++ FypTest_APP/viewController/SearchViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 import FirebaseDatabase
 
 class SearchViewController: UIViewController {
+    @IBOutlet weak var gymRoom1: UILabel!
+    @IBOutlet weak var gymRoom2: UILabel!
+    @IBOutlet weak var gymRoom3: UILabel!
+    @IBOutlet weak var gymRoom4: UILabel!
+    
+    @IBOutlet weak var searchBar: UITextField!
+    
     
     let database = Database.database().reference()
 
@@ -16,6 +23,23 @@ class SearchViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    @IBAction func searchButton(_ sender: Any) {
+        database.child("\(searchBar)").observeSingleEvent(of: .value, with:
+                                                            {snapshot in guard
+            let value = snapshot.value as? [String: Any] else {return}
+            
+            let gym1 = value["Gym1"] as! String
+            let gym2 = value["Gym2"] as! String
+            let gym3 = value["Gym3"] as! String
+            let gym4 = value["Gym4"] as! String
+            
+            self.gymRoom1.text = "1: \(gym1)"
+            self.gymRoom2.text = "2: \(gym2)"
+            self.gymRoom3.text = "3: \(gym3)"
+            self.gymRoom4.text = "4: \(gym4)"
+            
+        })
     }
     
 
