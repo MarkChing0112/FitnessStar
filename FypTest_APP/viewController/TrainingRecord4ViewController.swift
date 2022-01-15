4618296deac75ee96f479423e569ebb721d06c5c
Mark <manho445@gmail.com>
2022-01-15 23:19:32 +0800

    TrainingRecord&UI

---
+++ FypTest_APP/viewController/TrainingRecord4ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TrainingRecord4ViewController.swift
+//  FypTest_APP
+//
+//  Created by kin ming ching on 15/1/2022.
+//
+
+import UIKit
+
+class TrainingRecord4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
