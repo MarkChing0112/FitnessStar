71e538384722c859c072007fcd263c715a4256a8
TLok6844 <95626834+TLok6844@users.noreply.github.com>
2022-01-17 20:08:54 +0800

    kk
    ,

--- FypTest_APP/viewController/ViewController.swift
+++ FypTest_APP/viewController/ViewController.swift
@@ -55,6 +55,14 @@ class ViewController: UIViewController {
         view.layer.addSublayer(pointLayer)
         pointLayer.frame = view.frame
         pointLayer.strokeColor = UIColor.green.cgColor
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+           label.center = CGPoint(x: 160, y: 285)
+           label.textAlignment = .center
+           label.text = "I'm a test label"
+        
+        view.addSubview(label)
+        
     }
 
 }
