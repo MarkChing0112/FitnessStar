f9828958a888310a7980ebf62f773c5f26cb013a
Mark <manho445@gmail.com>
2022-03-13 16:42:07 +0800

    setting up the Signout constructor

--- FypTest_APP/viewController/Login_System/SignOut.swift
+++ FypTest_APP/viewController/Login_System/SignOut.swift
@@ -4,8 +4,18 @@
 //
 //  Created by Mark on 14/2/2022.
 //
-
+import UIKit
 import Foundation
+import Firebase
+import FirebaseAuth
 func SignOut(){
-
+    let firebaseAuth = Auth.auth()
+    do{
+    try firebaseAuth.signOut()
+          let firebaseAuth = Auth.auth()
+    
+      } catch let signOutError as NSError {
+        print("Error signing out: %@", signOutError)
+      }
 }
+
