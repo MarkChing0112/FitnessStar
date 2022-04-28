94eb7397333fedd391e7d7d1fec36d8d4cb48b00
isaaclee0928 <90861812+isaaclee0928@users.noreply.github.com>
2022-04-28 19:46:04 +0800

    CoreData2

---
+++ DistrictTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DistrictTableViewCell.swift
+//  FypTest_APP
+//
+//  Created by Isaac Lee on 28/4/2022.
+//
+
+import UIKit
+
+class DistrictTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
