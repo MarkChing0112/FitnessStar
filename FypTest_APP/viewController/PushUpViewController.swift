653006bc7fc29270f7b7b651d74b579a44a83ce9
okokli5 <manho445@gmail.com>
2022-05-03 21:04:50 +0800

    AI_View(PushUp,SitTraining Added)

---
+++ FypTest_APP/viewController/PushUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PushUpViewController.swift
+//  FypTest_APP
+//
+//  Created by kin ming ching on 3/5/2022.
+//
+
+import UIKit
+
+class PushUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FypTest_APP/viewController/PushUp_Predictor.swift
@@ -0,0 +1,8 @@
+//
+//  PushUp_Predictor.swift
+//  FypTest_APP
+//
+//  Created by kin ming ching on 3/5/2022.
+//
+
+import Foundation
