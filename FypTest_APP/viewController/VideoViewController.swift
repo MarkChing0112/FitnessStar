a3d5d24181c4526ac70fefdf6ddb10a01bb69a9b
isaaclee0928 <90861812+isaaclee0928@users.noreply.github.com>
2022-05-04 18:14:56 +0800

    Video

---
+++ FypTest_APP/viewController/VideoViewController.swift
@@ -0,0 +1,41 @@
+//
+//  VideoViewController.swift
+//  FypTest_APP
+//
+//  Created by Isaac Lee on 4/5/2022.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class VideoViewController: UIViewController {
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "now", ofType: "MOV")!))
+        
+        let layer = AVPlayerLayer(player: player)
+        layer.frame = view.bounds
+        view.layer.addSublayer(layer)
+        player.volume = 0
+        player.play()
+        
+        //use AVPlayerViewController
+        let vc = AVPlayerViewController()
+        vc.player = player
+        present(vc, animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
