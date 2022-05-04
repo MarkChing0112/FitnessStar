a7cb653cd538ac74ff684d267195f2149f6ba01a
okokli5 <manho445@gmail.com>
2022-05-04 17:36:10 +0800

    predictor AI_model

--- FypTest_APP/viewController/predictor.swift
+++ FypTest_APP/viewController/predictor.swift
@@ -8,7 +8,7 @@ import Vision
 //AImodel biceps file
 
 //set AI Model
-typealias ThrowingClassifies = Biceps_2
+typealias ThrowingClassifies = Biceps_3
 
 protocol PredictorDelegte: AnyObject {
     func predictor( predictor: Predictor,didFindNewRecognizedPoints point:[CGPoint])
