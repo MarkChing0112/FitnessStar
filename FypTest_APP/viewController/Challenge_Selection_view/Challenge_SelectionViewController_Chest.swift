f9b45334bbc9501c067eb5b91da56c31481a6472
okokli5 <manho445@gmail.com>
2022-05-04 18:16:30 +0800

    Challange_View_update

--- FypTest_APP/viewController/Challenge_Selection_view/Challenge_SelectionViewController_Chest.swift
+++ FypTest_APP/viewController/Challenge_Selection_view/Challenge_SelectionViewController_Chest.swift
@@ -6,24 +6,33 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
 
 class Challenge_SelectionViewController_Chest: UIViewController {
 
+    @IBOutlet weak var Time_limitLBL: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        GetUserData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func GetUserData(){
+        let ref = Database.database().reference()
+        let user = Auth.auth().currentUser
+        if let user = user {
+            ref.child("User_Challenge_Selection").child(user.uid).observeSingleEvent(of: .value, with: { snapshot in
+          // Get user value
+            let value = snapshot.value as? NSDictionary
+           // let body = value?["Bodypart"] as? String ?? ""
+            let Time_MINUS = value?["Time_Limit_Text"] as? String ?? ""
+            //show user selected session
+            self.Time_limitLBL.text = Time_MINUS
+        }) { error in
+          print(error.localizedDescription)
+        }
     }
-    */
+    }
 
 }
